802fd593f74dfe93b8ab0d1aa8f86c9322b53d48
Gordon Lin <contact@gordonlin.ca>
2020-09-13 18:05:42 -0400

    Updated app.js to clear session

--- server/app.js
+++ server/app.js
@@ -177,6 +177,10 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  req.session.destroy(function(err){
+    req.logout();
+  });
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
