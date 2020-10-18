1da0abf87dd8da502d43b7e74c8ec129d03fd24e
Gordon Lin <contact@gordonlin.ca>
2020-10-18 00:53:56 -0400

    Updated email message

--- bot/sendmail.js
+++ bot/sendmail.js
@@ -9,9 +9,9 @@ SendMail.sendMail = function (email, token, name){
         const msg = {
             to: email,
             from: process.env.FROM_EMAIL, // Use the email address or domain you verified above
-            subject: 'Massey Computer Science Club Verification',
-            text: `Hello ${name}!\n\nThank you for joining the Vincent Massey Secondary School Computer Science Club 2020-2021 Discord server.\n\nIn order to maintain member privacy and security, we are requiring all users to verify themselves with their student email. To complete your account verification, please visit ${link} .\n\nIf you are in need of any assistance, please contact a Discord administrator. We look forward to another year of computer science at Vincent Massey!`,
-            html: `Hello ${name}!<br><br>Thank you for joining the Vincent Massey Secondary School Computer Science Club 2020-2021 Discord server.<br><br>In order to maintain member privacy and security, we are requiring all users to verify themselves with their student email. To complete your account verification, please <a href="${link}">click here</a>.<br><br>If you are in need of any assistance, please contact a Discord administrator. We look forward to another year of computer science at Vincent Massey!`
+            subject: 'Vincent Massey Club Verification',
+            text: `Hello ${name}!\n\nThank you for joining a club here at Vincent Massey Secondary School on Discord.\n\nIn order to maintain member privacy and security, we are requiring all users to verify themselves with their student email. To complete your account verification, please visit ${link} .\n\nIf you are in need of any assistance, please contact a Discord administrator. We look forward to another year at Vincent Massey!`,
+            html: `Hello ${name}!<br><br>Thank you for joining a club here at Vincent Massey Secondary School on Discord.<br><br>In order to maintain member privacy and security, we are requiring all users to verify themselves with their student email. To complete your account verification, please <a href="${link}">click here</a>.<br><br>If you are in need of any assistance, please contact a Discord administrator. We look forward to another year at Vincent Massey!`
           };
 
         mailgun.messages().send(msg, function (error, body) {
